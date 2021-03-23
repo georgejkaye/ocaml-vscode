e4f602911f7ab61cffc44f76f6590ddfcb28bf74
George Kaye <georgejkaye@gmail.com>
2021-03-23 17:40:36 +0000

    Added compile script

---
+++ README.md
@@ -0,0 +1,1 @@
+# Scripts to use with OCaml and VS Code
---
+++ compile.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ $# -ne 1 ] ; then
+    echo "Usage: ./compile <module name>"
+    exit 0
+fi
+
+if ! hash pcregrep 2>/dev/null ; then
+    echo "pcregrep not installed"
+    exit 0
+fi
+
+# Grab packages from .merlin
+if [ -f ".merlin" ] ; then
+    PKGS=$(pcregrep -o1 'PKG ([a-z]*)' .merlin)
+fi
+
+for pkg in $PKGS ; do
+    if [ -z "$PACKAGES" ] ; then
+        PACKAGES=$pkg
+    else
+        PACKAGES=$PACKAGES,$pkg
+    fi
+done
+
+if [ ! -z "$PACKAGES" ] ; then
+    PKG_STRING="-pkgs $PACKAGES"
+fi
+
+OCAMLBUILD="ocamlbuild -use-ocamlfind $PKG_STRING src/$1.native"
+
+echo "Running $OCAMLBUILD"
+$OCAMLBUILD
+rm $1.native
